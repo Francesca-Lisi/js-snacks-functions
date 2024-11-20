a41fae2c182a0bdb5e427dfc8ef96d120fa052a0
Francesca-Lisi <francescalisi2143@gmail.com>
2024-11-20 22:47:04 +0100

    feat:  solve excercise 1 Iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,20 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e 
+restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+let newArray = []
 
 // Dichiara la funzione qui.
+function getFirstLetter (array){
+  for (let i = 0; i < array.length ; i++) {
+  newArray[i] = names[i].slice(0,1)
+  } return newArray
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(getFirstLetter(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
